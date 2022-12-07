1ac6969f13af679807b7609c32c42f22db515ba2
emil <emil.kantaneva@gmail.com>
2022-12-07 19:20:01 +0200

    modal popup centered

--- frontend/src/components/Common/Modal.js
+++ frontend/src/components/Common/Modal.js
@@ -2,12 +2,13 @@
 
 import React from "react";
 import PostBox from "../Posts/PostBox";
+import "./Modal.css";
 
 const Modal = (props) => {
 
   return (
     <>
-      <div className="absolute w-full h-full z-50">
+      <div className="modal">
         <div className="flex flex-col items-center justify-center">
           <PostBox user={props.user} from={props.origin} />
           <button>cancel</button>
